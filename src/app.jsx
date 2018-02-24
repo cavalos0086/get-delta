af05ecf893623830322d7eadce6614d921839aba
Cristian Avalos <cavalos@kargo.com>
2018-02-24 17:34:46 -0500

    renames app.js -> app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import REACTDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="container-fluid px-0">
+				<div className="jumbotron">
+					<h3>Coverage</h3>
+				</div>
+
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import App from './app';
+import App from './app.jsx';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
