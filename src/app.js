217aabb03cafb6e99f1ca7a80c2bc465567db296
Cristian Avalos <cavalos@kargo.com>
2018-02-23 22:07:26 -0500

    changes app title

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import REACTDOM from 'react-dom';
+
+export default class App extends React.Component {
+
+	render() {
+		return (
+			<div> Hello World from React</div>
+		);
+	}
+}
